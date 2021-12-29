a2e753ab4ccc1efab2b7a6605a70180af36b646b
rakis10 <jurajrak14@gmail.com>
2021-12-29 23:12:42 +0100

    token done, evaluate ma bad request 400

--- src/main/java/net/javaguides/springboot/config/SecurityConfiguration.java
+++ src/main/java/net/javaguides/springboot/config/SecurityConfiguration.java
@@ -1,36 +1,32 @@
 package net.javaguides.springboot.config;
 
+
 import net.javaguides.springboot.model.User;
+import net.javaguides.springboot.service.UserService;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.*;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import net.javaguides.springboot.service.UserService;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    Logger logger = LoggerFactory.getLogger(SecurityConfiguration.class);
 
     @Autowired
     private UserService userService;
@@ -73,6 +69,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .loginPage("/login")
                 .permitAll()
                 .successHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                    // call na risk server
+                    callRiskServer();
 
                     httpServletRequest.getSession().setAttribute("principal_name", authentication.getName());
                     httpServletRequest.getSession().setMaxInactiveInterval(300);
@@ -96,5 +94,46 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 .maximumSessions(1)
                 .expiredUrl("/login?error");
+    }
+
+    private void callRiskServer() {
+        String url = "https://serene-refuge-96326.herokuapp.com/oauth/token?scope=write&grant_type=password&username=foo&password=foo";
+        RestTemplate rt = new RestTemplate();
+
+        String plainCreds = "clientId:abcd";
+        byte[] plainCredsBytes = plainCreds.getBytes();
+        byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
+        String base64Creds = new String(base64CredsBytes);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Creds);
+
+        HttpEntity<String> request = new HttpEntity<String>(headers);
+        ResponseEntity<?> response = rt.exchange(url, HttpMethod.POST, request, JSONObject.class);
+//        JSONObject account = response.getBody();
+        JSONObject jsontoken = (JSONObject) response.getBody();
+        String token = (String) jsontoken.get("access_token");
+        logger.info(token);
+
+        evaluateRiskServer(token);
+
+    }
+
+    private void evaluateRiskServer(String token) {
+        String url = "https://serene-refuge-96326.herokuapp.com/evaluate";
+        RestTemplate rt = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Authorization", "Bearer " + token);
+//        JSONObject body = new JSONObject();
+//        body.put("lastIP", "192.16");
+//        body.put("lastTransaction", "create");
+        //  workaround
+        String body = "{\"lastIP\":\"129\",\"lastTransaction\":\"login\"}";
+        HttpEntity<String> request = new HttpEntity<String>( body.toString() ,headers);
+        ResponseEntity<?> response = rt.exchange(url, HttpMethod.GET, request, String.class);
+        logger.info((String) response.getBody());
+
+
     }
 }
